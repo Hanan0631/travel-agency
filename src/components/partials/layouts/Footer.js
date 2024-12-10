c7e99bf838a95eb21c28116b83b44cb74819ae0e
Hanan0631 <hm200390@gmail.com>
2024-12-10 16:29:06 +0330

    UI of layout added

--- src/components/partials/layouts/Footer.js
+++ src/components/partials/layouts/Footer.js
@@ -1,9 +1,87 @@
+//next
+import Image from "next/image";
+import Link from "next/link";
+
+//public
+import logo from "@/public/images/logo.png";
+import aira from "@/public/images/aira.png";
+import ecunion from "@/public/images/ecunion.png";
+import passengerRights from "@/public/images/passenger-rights.png";
+import samandehi from "@/public/images/samandehi.png";
+import stateAirline from "@/public/images/state-airline.png";
+
+//components
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className={styles.footer}>
+      <div className={styles.first}>
+        <div className={styles.right}>
+          <div className={styles.torino}>
+            <h3>تورینو</h3>
+            <ul>
+              <li>
+                <Link href="#">درباره ما</Link>
+              </li>
+              <li>
+                <Link href="#">تماس با ما</Link>
+              </li>
+              <li>
+                <Link href="#">چرا تورینو</Link>
+              </li>
+              <li>
+                <Link href="#">بیمه مسافرتی</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.services}>
+            <h3>خدمات مشتریان</h3>
+            <ul>
+              <li>
+                <Link href="#">پشتیبانی آنلاین</Link>
+              </li>
+              <li>
+                <Link href="#">راهنمای خرید</Link>
+              </li>
+              <li>
+                <Link href="#">راهنمای استرداد</Link>
+              </li>
+              <li>
+                <Link href="#">پرسش و پاسخ</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Image src={logo} width={100} height={30} alt="Logo" />
+            <p>تلفن پشتیبانی: 8574-021</p>
+          </div>
+          <div className={styles.images}>
+            <Image src={aira} width={35} height={38} alt="aira" />
+            <Image src={ecunion} width={34} height={38} alt="ecunion" />
+            <Image
+              src={passengerRights}
+              width={36}
+              height={38}
+              alt="passenger rights"
+            />
+            <Image src={samandehi} width={34} height={38} alt="samandehi" />
+            <Image
+              src={stateAirline}
+              width={40}
+              height={38}
+              alt="state airline"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.second}>
+        <p>کلیه حقوق این وب سایت متعلق به تورینو میباشد.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/partials/layouts/Header.js
+++ src/components/partials/layouts/Header.js
@@ -1,10 +1,24 @@
 "use client";
 
-import SignInButton from "@/ui/atom/SignInButton";
-import styles from "./Header.module.css";
-import MenuButton from "@/ui/atom/MenuButton";
+//next
+import Link from "next/link";
+import Image from "next/image";
+
+//react
 import { useEffect, useRef, useState } from "react";
+
+//public
+import logo from "@/public/images/logo.png"
+
+//components
+import SignInButton from "@/ui/atom/SignInButton";
+import MenuButton from "@/ui/atom/MenuButton";
 import Navbar from "@/ui/organism/Navbar";
+import LoginButton from "@/ui/molecule/LoginButton";
+
+//styles
+import styles from "./Header.module.css";
+
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,12 +41,24 @@ function Header() {
   return (
     <div className={styles.header}>
       {isOpen && <Navbar reference={ref} />}
-      <div onClick={() => setIsOpen(true)}>
+      <div className={styles.mobileDesign} onClick={() => setIsOpen(true)}>
         <MenuButton />
       </div>
-      <div>
+      <div className={styles.mobileDesign}>
         <SignInButton />
       </div>
+      <div className={styles.desktopDesign}>
+        <Image src={logo} width={146} height={44} alt="Logo" />
+        <ul>
+          <Link href="/">صفحه اصلی</Link>
+          <Link href="/">خدمات گردشگری</Link>
+          <Link href="/">درباره ما</Link>
+          <Link href="/">تماس با ما</Link>
+        </ul>
+      </div>
+      <Link className={styles.desktopDesign} href="/send-otp">
+        <LoginButton />
+      </Link>
     </div>
   );
 }
